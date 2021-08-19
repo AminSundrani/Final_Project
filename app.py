c9be0a32515d5d4cacc1526373c7a3a19a8762a3
Amin Sundrani <amin.sundrani@outlook.com>
2021-08-19 22:03:34 +0800

    Final_Project

--- app.py
+++ app.py
@@ -1,29 +1,32 @@
-# Import libraries
+
 import numpy as np
 from flask import Flask, request, jsonify, render_template
-import pickle
+import joblib
 
 app = Flask(__name__)
-
-# Load the model
-model = pickle.load(open('movies_model_overall.pkl','rb'))
+# model = pickle.load(open('movies_model_overall.pkl', 'rb'))
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
-
-@app.route('/api',methods=['POST'])
+@app.route('/prediction',methods=['GET','POST'])
 def predict():
-    # Get the data from the POST request.
-    data = request.get_json(force=True)
-    print(data)
-    # Make prediction using model loaded from disk as per the data.
-    prediction = model.predict([[(data['values'])]])
-    print(prediction)
-    # Take the first value of prediction
-    output = prediction[0][0]
-    return jsonify({"result" : output})
 
-if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+   if request.method == "POST" : 
+       data = request.form.get('input1')
+       data = np.array(data)
+       data = data.reshape(1,-1)
+        #open file
+       file = open("movies_model_overall.pkl","rb")
+    
+        #load trained model
+       trained_model = joblib.load(file)
+    
+        #predict
+       prediction = trained_model.predict(data)
+    
+       return render_template('index.html', prediction = prediction[0][0])
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import requests
 
 url = 'http://localhost:5000/api'
-r = requests.post(url,json={'values': 20000000})
+r = requests.post(url,json={'values'})
 print(r.json())
